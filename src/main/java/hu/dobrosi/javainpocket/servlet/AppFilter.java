f1a769fd80fa8e6d0bc8d05704a11d9c7bc6a180
andris <andris@freemail.hu>
2016-01-01 14:22:07 +0100

    bla

---
+++ src/main/java/hu/dobrosi/javainpocket/servlet/AppFilter.java
@@ -0,0 +1,18 @@
+package hu.dobrosi.javainpocket.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class AppFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		chain.doFilter(request, response);
+	}
+}
